813de2fc7d65bb31fd2c33803a8550a2e576c7aa
tkolev <todor@devflow.tools>
2023-09-23 19:38:11 +0100

    Revert "Revert "Remove optional parameter""
    This reverts commit 3e4a5d9e819f0242b7bcb355c4c8b70e1eeec14a.

--- src/main/java/io/pactflow/example/kafka/Product.java
+++ src/main/java/io/pactflow/example/kafka/Product.java
@@ -13,15 +13,13 @@ class Product {
   private String type;
   private String version;
   private String event;
-  private String optParam;
 
   Product() {}
-  Product(String id, String name, String type, String version, String event, String optParam) {
+  Product(String id, String name, String type, String version, String event) {
     this.id = id;
     this.name = name;
     this.type = type;
     this.version = version;
     this.event = event;
-    this.optParam = optParam;
   }
 }
